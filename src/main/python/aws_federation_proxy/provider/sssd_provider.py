37dcf422383570840c200d418851c8248d8695b3
Sebastian Martinka <sebastian.martinka@mercateo.com>
2017-04-19 08:37:54 +0100

    added debug logging to new provider

--- src/main/python/aws_federation_proxy/provider/sssd_provider.py
+++ src/main/python/aws_federation_proxy/provider/sssd_provider.py
@@ -4,9 +4,14 @@
 from aws_federation_proxy.provider import ProviderByGroups
 
 import pysss
+import logging
+logging.basicConfig(filename='/var/log/python/debug.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
 
 class Provider(ProviderByGroups):
     """Uses the pysss module to retrieve group information from SSSD"""
 
     def get_group_list(self):
-return pysss.getgrouplist(self.user)
+        logging.debug("start & finished sssd_provider.get_group_list")
+        return pysss.getgrouplist(self.user)
